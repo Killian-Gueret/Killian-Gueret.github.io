61aa1a41c49ffd8bc0b3959a49b17fe555632bba
Killian Guéret <gueret-k@outlook.com>
2025-06-27 19:43:53 +0200

    `Edit functiion button_page, added comment. Add @media scheme dark-light for light theme user and dark theme user, add filter to change object svg color to match theme`

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 $(".footer").load("/pages/footer.html")
 
-function button_page(string)
-{$(document).on("click", `.${string}`, ()=>{window.location.replace(`./pages/${string}.html`)})}
+/**
+ * Ajoute une action à tous les éléments ayant la classe spécifiée.
+ * Lorsqu'un de ces éléments est cliqué, l'utilisateur est redirigé vers une page HTML correspondante.
+ *
+ * @param {string} pageName - Nom de la classe et du fichier HTML cible (sans extension).
+ * Exemple : button_page("contact") écoutera les clics sur les éléments avec la classe .contact
+ *           et redirigera vers "./pages/contact.html".
+ */
+function button_page(pageName)
+{$(document).on("click", `.${pageName}`, ()=>{window.location.replace(`./pages/${pageName}.html`)})}
 
-button_page("about"), button_page("skills"), button_page("experiences"), button_page("projects")
+["about", "skills", "experiences", "projects"].forEach(button_page);
 $(document).on("click", ".return", ()=>{window.location.replace("/index.html")})
